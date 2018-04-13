246266fda2408210ad5d6ab1127c881c2a9a45cf
Julius <13006605813@163.com>
2018-04-13 10:30:43 +0800

    remove obsolete files

--- chatServer/protoloader.lua
+++
@@ -1,8 +0,0 @@
-local skynet = require "skynet"
-local proto = require "proto"
-local protoloader = require "sprotoloader"
-
-skynet.start(function()
-    protoloader.save(proto.c2s,1)
-    protoloader.save(proto.s2c,2)
-end)
